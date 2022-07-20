776f137f3f3fd1eaaadf75c3e3c21385f01bca38
ashrafulabir1 <ashrafulabir.nsu@gmail.com>
2022-07-20 14:32:37 +0600

    update linear search

--- src/Default/LinearSearch.java
+++ src/Default/LinearSearch.java
@@ -4,14 +4,20 @@ public class LinearSearch {
 	   public static void main(String args[]){
 	      int array[] = {10, 20, 25, 63, 96, 57};
 	      int size = array.length;
-	      int value = 63;
-
-	      for (int i=0 ;i< size; i++){
-	         if(array[i]==value){
-	            System.out.println("Search element is: "+value+" found at the index of "+ i);
-	         }else{
-	            System.out.println("Element not found");
-	         }
-	      }
+	      int value1 = 63;
+	      int value2= 57;
+	    
+	    	 for (int i=0 ;i< size; i++){
+		         if(array[i]==value1 || (array[i]==value2 && array[i]==value1)){
+		            System.out.println("Search element is: "+value1+" found at the index of "+ i);
+		         }
+		            else if(array[i]==value2 || (array[i]==value2 && array[i]==value1)) {
+		            	System.out.println("Search element is: "+value2+" found at the index of "+ i);
+		            }
+		         else{
+		            System.out.println("Element not found");
+		         
+		      }
+	     }
 	   }
 	}
